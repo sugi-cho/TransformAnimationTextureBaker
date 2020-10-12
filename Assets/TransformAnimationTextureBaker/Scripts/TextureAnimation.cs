63de29f9ab0e50d6ddfa1e7ede94b9f8ac7426c6
hironori sugino <git@sugi.cc>
2020-10-13 06:19:04 +0900

    add texture baker and player ShaderGraph

---
+++ Assets/TransformAnimationTextureBaker/Scripts/TextureAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TextureAnimation : MonoBehaviour
+{
+    MaterialPropertyBlock mpb { get { if (_mpb == null) _mpb = new MaterialPropertyBlock(); return _mpb; } }
+    MaterialPropertyBlock _mpb;
+
+    readonly int PropIndex = Shader.PropertyToID("_Idx");
+    readonly int PropTime = Shader.PropertyToID("_T");
+
+    public Renderer[] renderers;
+    public int fps = 30;
+    public float t;
+
+    private void Reset()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+    }
+
+    private void OnValidate()
+    {
+        SetProp();
+    }
+
+    void SetProp()
+    {
+        for (var i = 0; i < renderers.Length; i++)
+        {
+            SetFloat(renderers[i], PropIndex, i);
+            SetFloat(renderers[i], PropTime, t*fps);
+        }
+    }
+
+    void SetFloat(Renderer r, int propId, float val)
+    {
+        r.GetPropertyBlock(mpb);
+        mpb.SetFloat(propId, val);
+        r.SetPropertyBlock(mpb);
+    }
+}
---
+++ Assets/TransformAnimationTextureBaker/Scripts/TransformAnimationTextureBaker.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TransformAnimationTextureBaker : MonoBehaviour
+{
+    public Transform transformRoot;
+    public GameObject animationRoot;
+    public AnimationClip[] clips;
+    public int fps = 30;
+
+    Transform[] transforms;
+
+    [ContextMenu("bake texture")]
+    void Bake()
+    {
+        if (transformRoot == null || clips.Length < 1)
+            return;
+        transforms = transformRoot.GetComponentsInChildren<Renderer>().Select(r => r.transform).ToArray();
+        var trsCount = transforms.Length;
+
+        foreach(var c in clips)
+        {
+            var length = c.length;
+            var height = Mathf.NextPowerOfTwo((int)(length * fps));
+            var dt = length / (height - 1);
+
+            var posTex = CreateBakeTexture(trsCount, height, c.isLooping);
+            var rotTex = CreateBakeTexture(trsCount, height, c.isLooping);
+            var scaleTex = CreateBakeTexture(trsCount, height, c.isLooping);
+
+            for(var i = 0; i < height; i++)
+            {
+                var t = i * dt;
+                c.SampleAnimation(animationRoot, t);
+                for(var n = 0; n < trsCount; n++)
+                {
+                    var trs = transforms[n];
+                    var pos = transformRoot.InverseTransformPoint(trs.position);
+                    var rot = trs.rotation;
+                    var scale = trs.lossyScale;
+                    posTex.SetPixel(n, i, new Color(pos.x, pos.y, pos.z));
+                    rotTex.SetPixel(n, i, new Color(rot.x, rot.y, rot.z, rot.w));
+                    scaleTex.SetPixel(n, i, new Color(scale.x, scale.y, scale.z));
+                }
+            }
+
+#if UNITY_EDITOR
+            AssetDatabase.CreateAsset(posTex, $"Assets/{animationRoot.name}_{c.name}_posTex.asset");
+            AssetDatabase.CreateAsset(rotTex, $"Assets/{animationRoot.name}_{c.name}_rotTex.asset");
+            AssetDatabase.CreateAsset(scaleTex, $"Assets/{animationRoot.name}_{c.name}_scaleTex.asset");
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+#endif
+        }
+    }
+
+    Texture2D CreateBakeTexture(int w, int h, bool isLooping)
+    {
+        return new Texture2D(w, h, TextureFormat.RGBAHalf, false)
+        {
+            wrapModeU = TextureWrapMode.Clamp,
+            wrapModeV = isLooping ? TextureWrapMode.Repeat : TextureWrapMode.Clamp,
+            filterMode = FilterMode.Bilinear,
+        };
+    }
+}
